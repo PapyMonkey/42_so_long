ed7086765a024a35e8e1be2b5ffdee0cf7935f92
Anthony Guiri <exastir@icloud.com>
2022-05-23 09:29:22 +0200

    docs(images): add documentation and restruct folders

--- include/images.h
+++ include/images.h
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   win_management.h                                   :+:      :+:    :+:   */
+/*   images.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 16:57:24 by aguiri            #+#    #+#             */
-/*   Updated: 2022/05/23 05:30:26 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/05/23 09:28:09 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WIN_MANAGEMENT_H
-# define WIN_MANAGEMENT_H
+#ifndef IMAGES_H
+# define IMAGES_H
 
 # include "structures.h"
 
 // *****************************************************************************
-// Functions - Windows and images mangement related
+// Functions - img_create.c
 
+/**
+ * @brief Create a MLX image, store it in a structure and return it.
+ *
+ * @param var Variable containing all the other useful ones.
+ * @param width Width of the image to be created.
+ * @param height Height of the image to be created.
+ * @return Image stored in a custom structure.
+ */
 t_img	*img_create(t_mlx *var, int width, int height);
 
-#endif		// ifndef WIN_MANAGEMENT_H
+#endif		// ifndef IMAGES_H
