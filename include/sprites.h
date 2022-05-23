5ed8db2638bc3615280af2dc4279dc6a4a399716
Anthony Guiri <exastir@icloud.com>
2022-05-23 09:24:14 +0200

    docs(sprites): add documentation

--- include/sprites.h
+++ include/sprites.h
@@ -6,7 +6,7 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 16:44:58 by aguiri            #+#    #+#             */
-/*   Updated: 2022/05/23 04:36:08 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/05/23 09:23:34 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,25 @@
 # include "structures.h"
 
 // ****************************************************************************
-// Functions - Sprites
+// Functions - sp_background.c
 
+/**
+ * @brief Create an image containing the background and put it to the main
+ * 		window.
+ *
+ * @param var Variable containing all the other useful ones.
+ */
 void	sp_background(t_mlx *var);
 
+// ****************************************************************************
+// Functions - sp_background.c
+
+/**
+ * @brief Create an image of the player thanks to his current position and put it
+ *		to the main window.
+ *
+ * @param var Variable containing all the other useful ones.
+ */
 void	sp_player(t_mlx *var);
 
 #endif // ifndef SRITES_H
